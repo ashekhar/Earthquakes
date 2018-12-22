b0e8f597142ee0f0ba9f9ed31039cc509187af4f
ashekhar <anilshekhar@hotmail.com>
2018-12-21 17:45:46 -0800

    Update quakes.java
    Replaced String with StringBuilder() - for performance with large amount of data.

--- quakes.java
+++ quakes.java
@@ -3,29 +3,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-// https://android.jlelse.eu/a-performance-comparison-of-treemap-and-priorityqueue-in-a-certain-use-case-d9c2979276a1
+/*
+ * GeoJSON Summary Format
+ * https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+ */
 
 /**
  * @author ashekhar
@@ -84,16 +80,16 @@ class EarthquakeMagComparator<T1,T2 extends Comparable<T2>> implements Comparato
  */
 public class quakes {
 
-	final private static String USA_STATE_DATA = "C:\\Users\\ashekhar\\workspace\\Earthquakes\\src\\StateNamesAndCodes.csv";
-	final private static String USGS_EARTHQUAKE_DATA_API = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+	final private static String USA_STATE_DATA 				= "C:\\Users\\ashekhar\\workspace\\Earthquakes\\src\\StateNamesAndCodes.csv";
+	final private static String USGS_EARTHQUAKE_DATA_API 	= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 	
-	final private static int TOP_US_STATES_NUMBER_OF_EARTHQUAKES = 5;
-	final private static int TOP_STRONGEST_EARTHQUAKES_IN_STATE = 25;
+	final private static int TOP_US_STATES_NUMBER_OF_EARTHQUAKES 	= 5;
+	final private static int TOP_STRONGEST_EARTHQUAKES_IN_STATE 	= 25;
 
-	private static HashSet<String> NamesOfTerritoriesAndStates = new HashSet<String>();
-	private static HashSet<String> CodesOfTerritoriesAndStates = new HashSet<String>();
-	private static Map<String, String> StateNames2Codes = new HashMap<String, String>();
-	private static Map<String, String> StateCodes2Names = new HashMap<String, String>();
+	private static HashSet<String> NamesOfTerritoriesAndStates 	= new HashSet<String>();
+	private static HashSet<String> CodesOfTerritoriesAndStates 	= new HashSet<String>();
+	private static Map<String, String> StateNames2Codes 		= new HashMap<String, String>();
+	private static Map<String, String> StateCodes2Names 		= new HashMap<String, String>();
 
 	/**
 	 * Read the state/territory names and codes to build up the data structure.
@@ -137,7 +133,7 @@ public class quakes {
 	}
 
 	/**
-	 * Verify if the earthquake event occured within USA or not.
+	 * Verify if the earthquake event occurred within USA or not.
 	 * @param stateStr
 	 * @return boolean
 	 */
@@ -168,14 +164,15 @@ public class quakes {
 	 */
 	private static String validateInputArguments(String[] args) {
 
-		Class thisClass = new Object(){}.getClass();
-		String className = thisClass.getEnclosingClass().getSimpleName();
+		// Get the name of the current program/class
+		Class thisClass		= new Object(){}.getClass();
+		String className	= thisClass.getEnclosingClass().getSimpleName();
 		
 		String firstArg = null;
 		
 		if (args.length == 1) {
 			
-			// atleast need 3 characters to proceed
+			// at-least need 3 characters to proceed
 			if (args[0].length() > 3) {
 			
 				if (args[0].substring(0, 2).equals("--")) {
@@ -213,9 +210,9 @@ public class quakes {
 		PriorityQueue<EarthquakeDataNode> sortedStateEarthquakeData = new PriorityQueue<EarthquakeDataNode>(100, EarthquakeDataComparator);
 
 		// Overall top 5 states by number of earthquakes
-		Map<String, Integer> earthquakeCountPerState = new HashMap<String, Integer>();
-		EarthquakeMagComparator comp = new EarthquakeMagComparator(earthquakeCountPerState);
-        TreeMap<String,Integer> earthquakeMagnitudeSortedMap = new TreeMap<String,Integer>(comp);
+		Map<String, Integer> earthquakeCountPerState			= new HashMap<String, Integer>();
+		EarthquakeMagComparator comp							= new EarthquakeMagComparator(earthquakeCountPerState);
+        TreeMap<String,Integer> earthquakeMagnitudeSortedMap	= new TreeMap<String,Integer>(comp);
 
 		// Get earthquake data (API response)
 		String earthquakesData = quakesHelperMethods.getEarthquakesData(USGS_EARTHQUAKE_DATA_API);
@@ -225,9 +222,9 @@ public class quakes {
 			JSONObject mainJSONObj = new JSONObject(earthquakesData);
 
 			// Parse
-			JSONObject metadataObj = mainJSONObj.getJSONObject("metadata");
-			int getStatus = metadataObj.getInt("status");
-			int getDataCount = metadataObj.getInt("count");
+			JSONObject metadataObj	= mainJSONObj.getJSONObject("metadata");
+			int getStatus			= metadataObj.getInt("status");
+			int getTotalDataCount	= metadataObj.getInt("count");
 
 			// Verify the API status code
 			if (getStatus == 200) {
@@ -235,10 +232,13 @@ public class quakes {
 				JSONArray readingsJSONArray = mainJSONObj.getJSONArray("features");
 
 				// Loop through the earthquake data
-				for (int i = 0; i < getDataCount; i++) {
+				JSONObject reading		= null;
+				JSONObject properties	= null;
+				
+				for (int i = 0; i < getTotalDataCount; i++) {
 
-					JSONObject reading = readingsJSONArray.getJSONObject(i);
-					JSONObject properties = reading.getJSONObject("properties");
+					reading		= readingsJSONArray.getJSONObject(i);
+					properties	= reading.getJSONObject("properties");
 
 					// Focus only on earthquake data
 					if (properties.getString("type").equals("earthquake")) {
@@ -252,13 +252,14 @@ public class quakes {
 						 * title: M 0.5 - 9km NE of Aguanga, CA Parse the title
 						 * to get the magnitude value
 						 */
-						String magStr = properties.getString("title").split(" - ")[0].split(" ")[1];
-						float magFloat;
+
+						StringBuilder magStr = new StringBuilder();
+						magStr.append(properties.getString("title").split(" - ")[0].split(" ")[1]);
 
 						// Ignore if mag value is null or if the mag value in
 						// title is ?
-						if (!magStr.equals("?")) {
-							magFloat = Float.parseFloat(magStr);
+						if (!magStr.toString().equals("?")) {
+							float magFloat = Float.parseFloat(magStr.toString());
 
 							/*
 							 * Invalid title: M 4.5 - Federated States of
@@ -266,30 +267,36 @@ public class quakes {
 							 * state component in the title.
 							 */
 							if (properties.getString("title").split(", ").length == 2) {
-								String stateStr = properties.getString("title").split(", ")[1];
 
+								StringBuilder stateStr = new StringBuilder();
+								stateStr.append(properties.getString("title").split(", ")[1]);
+								
 								// Validate if the data is indeed in the USA
-								if (isEarthquakeWithinUSA(stateStr)) {
+								if (isEarthquakeWithinUSA(stateStr.toString())) {
 
-									stateStr = convertStateCode2NameIfNeeded(stateStr);
+									StringBuilder stateStrUpdated = new StringBuilder();
+									stateStrUpdated = stateStrUpdated.append(convertStateCode2NameIfNeeded(stateStr.toString()));
 
 									// Build earthquake data object
-									EarthquakeDataNode eqData = new EarthquakeDataNode();
-									eqData.magnitude = magFloat;
-									eqData.magnitudeType = properties.getString("magType");
-									eqData.time = properties.getLong("time");
-									eqData.summaryWithLocation = properties.getString("title");
+									EarthquakeDataNode eqData	= new EarthquakeDataNode();
+									eqData.magnitude			= magFloat;
+									eqData.magnitudeType		= properties.getString("magType");
+									eqData.time					= properties.getLong("time");
+									eqData.summaryWithLocation	= properties.getString("title");
 
 									if (firstArg.equalsIgnoreCase("top5")) {
+										
 										// Keep track of number of earthquakes in each state.
-										if (earthquakeCountPerState.containsKey(stateStr)) {
-											earthquakeCountPerState.put(stateStr, earthquakeCountPerState.get(stateStr) + 1);
+										if (earthquakeCountPerState.containsKey(stateStrUpdated.toString())) {
+											earthquakeCountPerState.put(stateStrUpdated.toString(), earthquakeCountPerState.get(stateStrUpdated.toString()) + 1);
 										} else {
-											earthquakeCountPerState.put(stateStr, 1);
+											earthquakeCountPerState.put(stateStrUpdated.toString(), 1);
 										}
+										
 									} else {
+										
 										firstArg = convertStateCode2NameIfNeeded(firstArg);
-										if (firstArg.equalsIgnoreCase(stateStr))
+										if (firstArg.equalsIgnoreCase(stateStrUpdated.toString()))
 											sortedStateEarthquakeData.add(eqData);
 									}
 								}
@@ -302,10 +309,15 @@ public class quakes {
 				Date date = new Date();
 				
 				if (firstArg.equalsIgnoreCase("top5")) {
+					
+					//Do this ("putAll") ONLY if the argument is "top5"
 					earthquakeMagnitudeSortedMap.putAll(earthquakeCountPerState);
+					
 					System.out.println("A list of the top 5 US states by number of earthquakes, highest to lowest. (As of " + dateFormat.format(date) + ")");
 					quakesHelperMethods.pollEarthquakeDataFromTreeMap(earthquakeMagnitudeSortedMap, TOP_US_STATES_NUMBER_OF_EARTHQUAKES);
+					
 				} else {
+					
 					System.out.println("A list of the top 25 strongest earthquakes in " + firstArg + ", highest to lowest. (As of " + dateFormat.format(date) + ")");
 					System.out.println("Earthquakes reported for state: " + firstArg + " (Total number of recordings: " + sortedStateEarthquakeData.size() + ")");
 					quakesHelperMethods.pollEarthquakeDataFromPriorityQueue(sortedStateEarthquakeData, TOP_STRONGEST_EARTHQUAKES_IN_STATE);
