af4ebc0ac9b3dec0f88e6d30b14a36868ee4c003
ashekhar <anilshekhar@hotmail.com>
2018-12-21 20:57:00 -0800

    Update quakesHelperMethods.java
    Cosmetic update

--- quakesHelperMethods.java
+++ quakesHelperMethods.java
@@ -1,35 +1,42 @@
-import java.io.BufferedReader;
+import java.net.URL;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.ProtocolException;
-import java.net.URL;
-import java.util.Calendar;
+import java.net.MalformedURLException;
+
 import java.util.Map;
+import java.util.Calendar;
 import java.util.PriorityQueue;
 
 public class quakesHelperMethods {
 
+	/**
+	 * @param className
+	 * @param args0
+	 */
 	static void usage(String className, String args0) {
+		
 		System.err.println("Usage: java " + className + " --top5 | --<Name of state | State initials>");
 		System.out.println("For the following states please use the state code.");
-		System.out.println("\tNew Hampshire - NH"); 
-		System.out.println("\tNew Jersey - NJ"); 
-		System.out.println("\tNew Mexico - NM"); 
-		System.out.println("\tNew York - NY"); 
-		System.out.println("\tNorth Carolina - NC"); 
-		System.out.println("\tNorth Dakota - ND"); 
-		System.out.println("\tRhode Island - RI"); 
-		System.out.println("\tSouth Carolina - SC"); 
-		System.out.println("\tSouth Dakota - SD"); 
-		System.out.println("\tWest Virginia - WV"); 
+		System.out.println("\tNew Hampshire (NH)"); 
+		System.out.println("\tNew Jersey (NJ)"); 
+		System.out.println("\tNew Mexico (NM)"); 
+		System.out.println("\tNew York (NY)"); 
+		System.out.println("\tNorth Carolina (NC)"); 
+		System.out.println("\tNorth Dakota (ND)"); 
+		System.out.println("\tRhode Island (RI)"); 
+		System.out.println("\tSouth Carolina (SC)"); 
+		System.out.println("\tSouth Dakota (SD)"); 
+		System.out.println("\tWest Virginia (WV)"); 
 
 		System.exit(-1);
 	}
 
 	/**
 	 * Print the top 5 entries in the priority queue
+	 * 
 	 * @param pQueue
 	 */
 	static void pollEarthquakeDataFromPriorityQueue(PriorityQueue<EarthquakeDataNode> pQueue, int count) {
@@ -46,7 +53,27 @@ public class quakesHelperMethods {
 	}
 
 	/**
+	 * Print the top earthquakes of a particular state
+	 * 
+	 * @param earthquakeCountPerState
+	 * @param count
+	 */
+	static void pollEarthquakeDataFromTreeMap(Map<String, Integer> earthquakeCountPerState, int count) {
+		
+		Integer upperLimit = earthquakeCountPerState.size() > count ? count : earthquakeCountPerState.size();
+		
+		Integer counter = 0;
+		for(String key : earthquakeCountPerState.keySet()) {
+			if (counter == upperLimit)
+				break;
+			System.out.println(key + ": " + earthquakeCountPerState.get(key));
+			counter++;
+		}
+	}
+
+	/**
 	 * Fetch the earthquake data by making the API call.
+	 * 
 	 * @param urlStr
 	 * @return
 	 */
@@ -95,22 +122,5 @@ public class quakesHelperMethods {
 		}
 
 		return result.toString();
-	}
-
-	/**
-	 * @param earthquakeCountPerState
-	 * @param count
-	 */
-	static void pollEarthquakeDataFromTreeMap(Map<String, Integer> earthquakeCountPerState, int count) {
-		
-		Integer upperLimit = earthquakeCountPerState.size() > count ? count : earthquakeCountPerState.size();
-		
-		Integer counter = 0;
-		for(String key : earthquakeCountPerState.keySet()) {
-			if (counter == upperLimit)
-				break;
-			System.out.println(key + ": " + earthquakeCountPerState.get(key));
-			counter++;
-		}
 	}
 }
