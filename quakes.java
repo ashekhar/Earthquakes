9099001f32916ff1bf11756dc7bb545da0a8614f
Anil Shekhar <anilshekhar@hotmail.com>
2018-12-23 16:15:25 -0800

    Update quakes.java
    Removed unused imports

--- quakes.java
+++ quakes.java
@@ -1,12 +1,3 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-
-import java.nio.file.Path;
-import java.nio.file.FileSystems;
-
 import java.util.Map;
 import java.util.Set;
 import java.util.Arrays;
